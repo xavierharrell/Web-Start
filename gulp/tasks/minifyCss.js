b7ebde7a6423570208378ce14027b26df2f6c412
Xavier Harrell <xavierharrell@gmail.com>
2016-07-31 08:08:40 -0500

    Changed how CSS is handled

--- gulp/tasks/minifyCss.js
+++ gulp/tasks/minifyCss.js
@@ -1,11 +1,11 @@
 var gulp      = require('gulp');
 var config    = require('../config').production;
-var minifyCSS = require('gulp-minify-css');
+var cleanCSS = require('gulp-clean-css');
 var size      = require('gulp-filesize');
 
 gulp.task('minifyCss', ['sass'], function() {
   return gulp.src(config.cssSrc)
-    .pipe(minifyCSS({keepBreaks:true}))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest(config.dest))
     .pipe(size());
 })
